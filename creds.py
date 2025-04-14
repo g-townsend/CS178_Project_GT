4950b836aeb54057c40eb43a2f993ebc2c93604d
EC2 Default User <ec2-user@ip-172-31-30-30.ec2.internal>
2025-04-14 16:06:27 +0000

    adding ec2 files to repo

---
+++ creds.py
@@ -0,0 +1,6 @@
+host = "project-one.cn9nbw41gsla.us-east-1.rds.amazonaws.com"
+user = "admin"
+password = "I<3CS178!"
+db = "world"
+
+
---
+++ projectdb.py
@@ -0,0 +1,58 @@
+import pymysql
+import creds 
+from flask import Flask
+app = Flask(__name__)
+
+def get_conn():
+    conn = pymysql.connect(
+        host= creds.host,
+        user= creds.user, 
+        password = creds.password,
+        db=creds.db,
+        )
+    return conn
+ 
+def execute_query(query, args=()):
+    cur = get_conn().cursor()
+    cur.execute(query, args)
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+
+
+#display the sqlite query in a html table
+def display_html(rows):
+    html = ""
+    html += """<table><tr><th>City</th><th>Population</th><th>Country</th></tr>"""
+
+    for r in rows:
+        html += "<tr><td>" + str(r[0]) + "</td><td>" + str(r[1]) + "</td><td>" + str(r[2]) + "</td></tr>"
+    html += "</table></body>"
+    return html
+
+
+@app.route("/languagequery/<language>")
+def viewdb(language):
+    rows = execute_query("""SELECT city.name, city.population, language
+                FROM name JOIN countrylanguage using (countrycode)
+                WHERE language = %s and percentage > 50
+                ORDER BY city.name
+                LIMIT 100""", (str(language))
+                )
+    return display_html(rows)
+
+from flask import request
+
+from flask import render_template
+@app.route("/languagequerytextbox", methods = ['GET'])
+def langauge_form():
+  return render_template('textbox.html', fieldname = "language")
+
+
+@app.route("/languagequerytextbox", methods = ['POST'])
+def language_form_post():
+  text = request.form['text']
+  return viewdb(text)
+
+if __name__ == '__main__': 
+    app.run(host = '0.0.0.0', port=8080, debug=True)
